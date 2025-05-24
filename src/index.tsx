0ac08db2f71ce54e6c51423624f5d30ca5f7008b
nibbl LLC <ben@nibbl.com>
2025-05-24 14:24:25 -0400

    fix to use javascript default props

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import SRLContextComponent from './SRL/SRLContext'
 import { useLightbox } from './SRL/SRLHooks'
 import SRLLightbox from './SRL/SRLLightbox'
 import SRLWrapper from './SRL/SRLWrapper'
 
-const SimpleReactLightbox = ({ children = null }) => {
+interface SimpleReactLightboxProps {
+  children?: React.ReactNode
+}
+
+const SimpleReactLightbox: React.FC<SimpleReactLightboxProps> = ({ children = null }) => {
   return (
     <SRLContextComponent>
       {children}
       <SRLLightbox />
     </SRLContextComponent>
   )
-}
-
-SimpleReactLightbox.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
 }
 
 export { SRLWrapper, useLightbox }
